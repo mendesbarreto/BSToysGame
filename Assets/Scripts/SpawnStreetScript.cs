0a905e919b897d9986e55367c8296601c71a0b59
IGDSorocaba <igdsorocaba@IGDSorocabas-Mac-mini.local>
2017-05-30 13:14:52 -0300

    add infinity road. add 3 pattern.

---
+++ Assets/Scripts/SpawnStreetScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnStreetScript : MonoBehaviour {
+
+	//VARIAÇÕES
+	public GameObject street1;
+	public GameObject street2;
+	public GameObject street3;
+	private GameObject mainStreet;
+
+	//CONTROLADOR DE REPETIÇÕES DE PISTA
+	[SerializeField]
+	private int timeChange = 3; 
+	private int cont = 0;
+
+	void Start(){
+		mainStreet = street1;
+	}
+		
+	void Update () {
+		ChangeStreet();
+		SpawnStreet ();
+	}
+		
+	void ChangeStreet(){
+		if (cont == timeChange && mainStreet == street1) 
+		{
+			mainStreet = street2;
+			cont = 0;
+		} 
+		else if (cont == timeChange && mainStreet == street2) 
+		{
+			mainStreet = street3;
+			cont = 0;
+		} 
+		else if (cont == timeChange && mainStreet == street3) 
+		{
+			mainStreet = street1;
+			cont = 0;
+		}
+	}
+
+
+	void SpawnStreet() {
+		if (StreetScript.isDestroyed == true) 
+		{
+			Vector3 spawnPoint = new Vector3 (transform.position.x, transform.position.y, transform.position.z-1);//LOCAL DO SPAWN
+			Instantiate (mainStreet, spawnPoint, Quaternion.identity);
+
+			StreetScript.isDestroyed = false;
+			cont++;
+		}
+
+	}
+}
---
+++ Assets/Scripts/StreetScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StreetScript : MonoBehaviour {
+
+    [SerializeField]
+    private float speedStreet = -1;
+	public static bool isDestroyed = false;
+			
+	void Update () {
+		gameObject.transform.Translate (0, 0, speedStreet); //MOVIMENTAÇÃO DA PISTA
+        Destroy();
+
+    }
+
+    void Destroy()
+    {
+        if (gameObject.transform.position.z == -100)
+		{
+			isDestroyed = true;
+			Destroy (gameObject);
+        }
+    }
+}
