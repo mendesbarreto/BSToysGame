34b168eaaa38bfb2d7cf7fda76526360f3187a44
IGDSorocaba <igdsorocaba@IGDSorocabas-Mac-mini.local>
2017-05-31 10:58:47 -0300

    Change StreetScript and SpawnStreetScript

---
+++ Assets/Scripts/MoveStreet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class MoveStreet : MonoBehaviour {
+
+	[SerializeField]
+	private float speedStreet = -1;
+	public static bool isDestroyed = false;
+	const float MAX_DISTANCE = -100;
+
+	private void Update () {
+		gameObject.transform.Translate (0, 0, speedStreet); //MOVIMENTAÇÃO DA PISTA
+		Destroy();
+	}
+
+	private void Destroy()
+	{
+		if (gameObject.transform.position.z == MAX_DISTANCE)
+		{
+			isDestroyed = true;
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/StreetBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class StreetBuilder : MonoBehaviour {
+
+	//VARIAÇÕES
+	public GameObject cityStreet;
+	public GameObject country;
+	public GameObject highway;
+	private GameObject mainStreet;
+
+	//CONTROLADOR DE REPETIÇÕES DE PISTA
+	[SerializeField]
+	private int timeChange = 3; 
+	private int cont = 0;
+
+	private void Start(){
+		mainStreet = cityStreet;
+	}
+
+	private void Update () {
+		ChangeStreet();
+		SpawnStreet ();
+	}
+
+	private void ChangeStreet(){
+		if (cont == timeChange && mainStreet == cityStreet) 
+		{
+			mainStreet = highway;
+			cont = 0;
+		} 
+		else if (cont == timeChange && mainStreet == highway) 
+		{
+			mainStreet = country;
+			cont = 0;
+		} 
+		else if (cont == timeChange && mainStreet == country) 
+		{
+			mainStreet = cityStreet;
+			cont = 0;
+		}
+	}
+
+
+	private void SpawnStreet() {
+		if (MoveStreet.isDestroyed == true) 
+		{
+			Vector3 spawnPoint = new Vector3 (transform.position.x, transform.position.y, transform.position.z-1);//LOCAL DO SPAWN
+			Instantiate (mainStreet, spawnPoint, Quaternion.identity);
+
+			MoveStreet.isDestroyed = false;
+			cont++;
+		}
+
+	}
+}
