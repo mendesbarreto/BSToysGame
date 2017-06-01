04251384b3169a9a9dfaec8765c0f995ea9b0838
Douglas Mendes <mendes-barreto@live.com>
2017-06-01 20:29:41 -0300

    Get streetBuilder using FindObjectOfType Change this to work with a single instace of StreetBuilder

--- Assets/Scripts/MoveStreet.cs
+++ Assets/Scripts/MoveStreet.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public sealed class MoveStreet : MonoBehaviour {
 
-	[SerializeField]
-	private float speedStreet = -1;
 	const float MAX_DISTANCE = -100;
 
+	private float SpeedStreet {
+		get {return speedStreet;}
+		set {speedStreet = value;}
+	}
+	[SerializeField]
+	private float speedStreet = -1;
+
+	private StreetBuilder StreetBuilder {
+		get{return streetBuilder;}
+		set{streetBuilder=value;}
+	}
+	private StreetBuilder streetBuilder;
+
+	private void Start() {
+		streetBuilder = GameObject.FindObjectOfType<StreetBuilder> ();
+	}
+
 	private void Update () {
+		if (isStreetDistanceAt(MAX_DISTANCE)) {
+			Destroy();
+		} else {
+			MoveStreetForward ();
+		}
+	}
+
+	private void MoveStreetForward() {
 		gameObject.transform.Translate (0, 0, speedStreet); //MOVIMENTAÇÃO DA PISTA
-		Destroy();
+	}
+
+	private bool isStreetDistanceAt(float z) 
+	{
+		if (gameObject.transform.position.z == z) 
+		{
+			return true;
+		}
+		return false;
 	}
 
 	private void Destroy()
 	{
-		if (gameObject.transform.position.z == MAX_DISTANCE)
-		{
-			StreetBuilder.isDestroyed = true;
-			Destroy (gameObject);
-		}
+		streetBuilder.IsDestroyed = true;
+		Destroy (gameObject);
 	}
 }
